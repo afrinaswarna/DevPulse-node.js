0d688245a48a9186cf25e33564bf376f39a92f91
swarna18021999 <afrinaswarna1999@gmail.com>
2026-07-14 16:16:21 +0600

    installed cors and global error handler

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,22 @@ import { signupRoute } from "./modules/auth/signup/signup.router";
 import { loginRoute } from "./modules/auth/login/login.route";
 import { userRoute } from "./modules/user/user.router";
 import { issuesRoute } from "./modules/issues/issues.route";
+import cors from "cors"
+import globalErrorHandler from "./modules/middleware/globalErrorHandler";
 
 const app: Application = express();
 
 app.use(express.json());
+
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  
+}
+
+app.use(cors(corsOptions ))
+
+
 
 
 app.get("/", (req: Request, res: Response) => {
@@ -22,6 +34,9 @@ app.use('/api/auth',signupRoute)
 app.use('/api/auth',loginRoute)
 app.use('/api/users',userRoute)
 app.use('/api/issues',issuesRoute)
+
+// Global Error Handling Middleware
+app.use(globalErrorHandler)
 
 
 
